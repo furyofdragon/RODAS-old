6f8722674f15b7435700cd36ebd99c5ea36f0830
furyofdragon <furyofdragon@gmail.com>
2013-11-27 01:30:21 +0400

    new class line

---
+++ RODAS/src/main/Line.java
@@ -0,0 +1,33 @@
+package main;
+
+public class Line {
+	
+	int id;
+	Point point1;
+	Point point2;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Point getPoint1() {
+		return point1;
+	}
+	
+	public void setPoint1(Point point1) {
+		this.point1 = point1;
+	}
+	
+	public Point getPoint2() {
+		return point2;
+	}
+	
+	public void setPoint2(Point point2) {
+		this.point2 = point2;
+	}
+
+}
--- RODAS/src/main/Point.java
+++ RODAS/src/main/Point.java
@@ -10,24 +10,31 @@ public class Point {
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public float getX() {
 		return x;
 	}
+	
 	public void setX(float x) {
 		this.x = x;
 	}
+	
 	public float getY() {
 		return y;
 	}
+	
 	public void setY(float y) {
 		this.y = y;
 	}
+	
 	public float getZ() {
 		return z;
 	}
+	
 	public void setZ(float z) {
 		this.z = z;
 	}
