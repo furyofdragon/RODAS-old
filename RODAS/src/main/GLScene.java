c96248b8425867fd1a8ff0023ac57621199eebfa
furyofdragon <furyofdragon@gmail.com>
2014-11-04 21:48:10 +0400

    return to project development

--- RODAS/src/main/GLScene.java
+++ RODAS/src/main/GLScene.java
@@ -19,8 +19,9 @@ public class GLScene implements GLEventListener {
 	* Called back by the animator to perform rendering.
 	*/
 	public void display(GLAutoDrawable drawable) {
-		GL2 gl = drawable.getGL().getGL2();
+		GL2 gl = drawable.getGL().getGL2();								// get the OpenGL graphics context
 		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);	// clear color and depth buffers
+		gl.glLoadIdentity();											// reset the model-view matrix
 		
 		/* colors
 		glColor3f(0.0, 0.0, 0.0);           black
@@ -67,7 +68,6 @@ public class GLScene implements GLEventListener {
 	* Called back before the OpenGL context is destroyed. Release resource such as buffers.
 	*/
 	public void dispose(GLAutoDrawable drawable) {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -78,6 +78,12 @@ public class GLScene implements GLEventListener {
 	public void init(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();		// get the OpenGL graphics context
 		glu = new GLU();						// get GL Utilities
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);// set background (clear) color -- black
+		gl.glClearDepth(1.0f);					// set clear depth value to farthest
+		gl.glEnable(GL2.GL_DEPTH_TEST);			// enables depth testing
+		gl.glDepthFunc(GL2.GL_LEQUAL);			// the type of depth test to do
+		gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST); // best perspective correction
+		gl.glShadeModel(GL2.GL_SMOOTH);			// blends colors nicely, and smoothes out lighting
 		gl.setSwapInterval(1);
 	}
 
@@ -88,17 +94,18 @@ public class GLScene implements GLEventListener {
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
 		GL2 gl = drawable.getGL().getGL2();		// get the OpenGL graphics context
 		if (height == 0) height = 1;			// prevent divide by zero
-		//float aspect = (float) width / height;
+		float aspect = (float) width / height;
 		
 		// Set the view port (display area) to cover the entire window
-		//gl.glViewport(0, 0, width, height);
+		gl.glViewport(0, 0, width, height);
 		
 		// Setup perspective projection, with aspect ratio matches viewport
-		//gl.glLoadIdentity();							// reset projection matrix
-		//glu.gluPerspective(45.0, aspect, 0.1, 100.0); 	// fovy, aspect, zNear, zFar
+		gl.glMatrixMode(GL2.GL_PROJECTION);				// choose projection matrix
+		gl.glLoadIdentity();							// reset projection matrix
+		glu.gluPerspective(45.0, aspect, 0.1, 100.0); 	// fovy, aspect, zNear, zFar
 		
 		// Enable the model-view transform
-		//gl.glLoadIdentity(); // reset
+		gl.glLoadIdentity(); // reset
 	}
 	
 	
